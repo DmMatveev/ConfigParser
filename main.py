a003b2e78d9b1f3cac875dbcae3c84fc6fc0891a
Shitestcoder <56564337+Shitestcoder@users.noreply.github.com>
2021-01-28 22:47:16 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import os
+
+#data = pd.read_("C:\\Users\\user\\Desktop\\ConfigParser\\SendMailService.exe.config", encoding='utf-8')
+
+os.chdir("C:\\Users\\user\\Desktop\\ConfigParser")
+print(os.getcwd())
